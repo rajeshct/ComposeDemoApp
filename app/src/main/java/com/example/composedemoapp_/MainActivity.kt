cdcfc5aba6ef5735d143cf3201f09c385c3713fa
Rajesh Tiwari <rajesh.tiwari02@sap.com>
2023-01-23 14:03:22 +0530

    Initial project setup

---
+++ app/src/main/java/com/example/composedemoapp_/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.composedemoapp_
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composedemoapp_.ui.theme.ComposeDemoApp_Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoApp_Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeDemoApp_Theme {
+        Greeting("Android")
+    }
+}
